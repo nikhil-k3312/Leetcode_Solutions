4435f8768a5c541096fe41b15b0261c1490702ae
Nikhil-K <nikhil.k3312@gmail.com>
2026-07-27 20:39:05 +0530

    Time: 4 ms (37.29%) | Memory: 43 MB (93.53%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '('||ch == '{'||ch == '['){
+                st.add(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                char tp = st.pop();
+                if(ch == ')' && tp!= '('||ch == ']' && tp!= '['||ch == '}' && tp!= '{'){
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
